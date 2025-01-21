eb0550acf80255ae779bb8355562320451f545aa
Albert-biorem <150436528+Albert-biorem@users.noreply.github.com>
2025-01-21 11:30:39 +0100

    Update Parallize.py

--- docs/notebooks/Parallize.py
+++ docs/notebooks/Parallize.py
@@ -1,33 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 21 10:17:39 2025
-
-@author: Albert
-"""
-
 import numpy as np
 import pandas as pd
 from joblib import Parallel, delayed
 import time
- 
-# record start time
+
+# Record start time
 start = time.time()
+
+# Load the DataFrame
 df = pd.read_csv("predictions/prediction_X_all_estriatum_points_41.tsv", sep="\t")
+
 # Initialize the zero array
 zero_array = np.zeros((340, 673, 552))
 
-# Define the function to process each element
-def process_element(i, j, t):
-    value = df[(df["pxl_row_transformed"] == i) & (df["pxl_column_transformed"] == j) & (df["slice"] == t)]["soft_prob_Striatal neuroblast"]
-    if not value.empty:
-        zero_array[i, j, t] = value.iloc[0]
+for i in range(340):
+    for j in range(673):
+        for t in range(1):
+            value = df[(df["pxl_row_transformed"] == i) & (df["pxl_column_transformed"] == j) & (df["slice"] == t)]["soft_prob_Striatal neuroblast"]
+            if not value.empty:
+               zero_array[i,j,t] =  value.iloc[0]
 
+# Update the zero_array with the results
 
-# Use joblib to parallelize the processing
-Parallel(n_jobs=-1)(delayed(process_element)(i, j, t) for i in range(340) for j in range(673) for t in range(2))
+# Record end time
 end = time.time()
- 
-# print the difference between start 
-# and end time in milli. secs
-print("The time of execution of above program is :",
-      (end-start) , "s")
+
+# Print the execution time
+print("The time of execution of above program is :", (end - start), "s")
